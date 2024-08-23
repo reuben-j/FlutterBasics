7fb96370fbfd9b242ab45b6f6d2e05f52089a0e8
reuben-j <reubenjarvela@gmail.com>
2024-08-23 09:38:49 +0100

    Strings

--- lib/testfile.dart
+++ lib/testfile.dart
@@ -1,13 +1,16 @@
 // https://dart-tutorial.com/introduction-and-basics/basic-dart-program/
 
 void main() {
-  print("Hello world");
+  print("Hello world \n");
   var name = "Reuben";
   print(name);
 
   int num1 = 10;
   int num2 = 25;
-
   print(num1 + num2);
+
+  String firstName = "Abi";
+  String surname = "Jarvela";
+  print("Hello $firstName $surname");
 }
 
