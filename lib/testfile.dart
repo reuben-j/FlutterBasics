6ecf6538accde093241228ff2653790f619e2bfb
reuben-j <reubenjarvela@gmail.com>
2024-08-23 09:36:01 +0100

    DartLearning

---
+++ lib/testfile.dart
@@ -0,0 +1,13 @@
+// https://dart-tutorial.com/introduction-and-basics/basic-dart-program/
+
+void main() {
+  print("Hello world");
+  var name = "Reuben";
+  print(name);
+
+  int num1 = 10;
+  int num2 = 25;
+
+  print(num1 + num2);
+}
+
