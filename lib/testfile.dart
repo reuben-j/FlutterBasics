9f39e5fbbd34000f85d52c232e4bd5ae998867d4
reuben-j <reubenjarvela@gmail.com>
2024-08-23 09:50:46 +0100

    DataTypes

--- lib/testfile.dart
+++ lib/testfile.dart
@@ -1,7 +1,8 @@
 // https://dart-tutorial.com/introduction-and-basics/basic-dart-program/
 
 void main() {
-  print("Hello world \n");
+  print("Hello world");
+  // variables can be declared with the specific int or string type or var and dart will figure it out
   var name = "Reuben";
   print(name);
 
@@ -12,5 +13,17 @@ void main() {
   String firstName = "Abi";
   String surname = "Jarvela";
   print("Hello $firstName $surname");
+
+  // dart data types:
+  String address = "Home";
+  int age = 12;
+  double bigNUm = 1234212.321;
+  // num can be an int or a floating point value
+  num newNum = 23.312;
+  bool iAmCool = true;
+  var anyVar = "yes";
+
+  print("$address $age $bigNUm $newNum $iAmCool $anyVar");
+
 }
 
