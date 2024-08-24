dac0559ed3d92f458c617d48483fb9fd0fc3244a
reuben-j <reubenjarvela@gmail.com>
2024-08-24 11:02:15 +0100

    dartConditionals

---
+++ lib/dart_conditionals.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  // if else statement
+  int age = 23;
+  if(age < 23) {
+    print("age is less than 23");
+  } else if(age == 23) {
+    print("age is 23!");
+   }
+    else {
+    print("age is above 23");
+  }
+
+  var time = 22;
+  assert(time!=22);
+}
